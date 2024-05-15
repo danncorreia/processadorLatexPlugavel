cad8ea0b5cd3fcb59e26ca6f817ea5fb77896791
Daniel Correia <danncorreia95@hotmail.com>
2024-05-15 09:21:41 +0100

    inicio connexao com mongodb

--- index.ts
+++ index.ts
@@ -1,31 +1,62 @@
 import { MyBase } from "./base";
-import { getTexFileContent } from "./utils";
+import {getArticlesFromDrive, getTexFileContent, processTexFilesInLatexFolders} from "./utils";
 import express, { Express, Request, Response } from "express";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
 const app: Express = express();
 const port = 3000;
+const uri = "mongodb+srv://application:87zLn69k0h3rIW5J@inprola.8m0y0z3.mongodb.net/?retryWrites=true&w=majority&appName=InProLa";
 
-app.get("/title", async (req: Request, res: Response) => {
-  const myBase = new MyBase({
-    texContent: await getTexFileContent('./latex/test article/main.tex'),
-  });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-  res.send(myBase.getTitleTag());
+app.get("/toProcessArticle", async (req: Request, res: Response) => {
+
+  getArticlesFromDrive();
+  // const myBase = new MyBase({
+  //   texContent: await getTexFileContent('./latex/test article/main.tex'),
+  // });
+  //
+  // res.send(myBase.getTitleTag());
 });
 
 app.get("/introduction", async (req: Request, res: Response) => {
-  const myBase = new MyBase({
-    texContent: await getTexFileContent('./latex/test article/main.tex'),
-  });
+  const myBase = new MyBase({})
 
-  res.send(myBase.getIntroduction());
+  processTexFilesInLatexFolders().then((listTexContents) => {
+    listTexContents.map((texContent) => {
+      const myBase = new MyBase({
+        texContent: texContent,
+      });
+    });
+  }).finally(() => {
+    res.send(myBase.getIntroduction());
+  });
 });
 
 app.get("/availabilityTest", async (req: Request, res: Response) => {
-  await new Promise(resolve => setTimeout(resolve, 10000));
-
-  res.send('Test completed!');
+res.send('Test completed!');
 });
 
 app.listen(port, () => {
+  run().catch(console.dir);
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
--- utils.ts
+++ utils.ts
@@ -1,4 +1,7 @@
-import * as fs from "fs";
+import * as fs from "fs"
+import * as path from 'path';
+import { google } from "googleapis";
+import { GoogleAuth } from "google-auth-library";
 
 export const getTexFileContent = async (path: string) => {
     let fileContent: string;
@@ -10,4 +13,39 @@ export const getTexFileContent = async (path: string) => {
 
     await fs.unlinkSync('./temp/convertedTex.txt');
     return fileContent;
-  }
+}
+
+export const processTexFilesInLatexFolders = async () => {
+    const files = []
+    const latexDir = './latex';
+    const directories = fs.readdirSync(latexDir, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+
+    for (const dir of directories) {
+        const texContent = await getTexFileContent(path.join(latexDir, dir, 'main.tex'));
+        files.push(texContent);
+    }
+
+    return files;
+}
+
+export const getArticlesFromDrive = async () => {
+    const auth = new GoogleAuth({
+        scopes: 'https://www.googleapis.com/auth/drive',
+    });
+    const service = google.drive({version: 'v3', auth});
+
+    const fileId = "1ZfRaMkRKFhMgBlF_s5ruZgA2yffV7Ncw";
+    try {
+        const file = await service.files.get({
+            fileId: fileId,
+            alt: 'media',
+        });
+        console.log(file.status);
+        return file.status;
+    } catch (err) {
+        // TODO(developer) - Handle error
+        throw err;
+    }
+}
